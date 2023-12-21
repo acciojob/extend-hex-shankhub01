5746d662ec410b9a35c7b44ff08ca592bde01e36
Shashank Tripathi <56106924+shankhub01@users.noreply.github.com>
2023-12-21 17:19:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const extendHex = (shortHex) => {
   // write your code here
+	
+  shortHex = shortHex.replace(/^#/, '');
+
+  // Split the short hex code into color components
+  const r = shortHex[0];
+  const g = shortHex[1];
+  const b = shortHex[2];
+
+  // Extend each color component to get the full hex code
+  const fullHex = `#${r}${r}${g}${g}${b}${b}`;
+
+  return fullHex.toUpperCase(); // Convert to uppercase for consistency
+}
+
+
+	
 };
 
 // Do not change the code below.
